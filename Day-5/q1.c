6a265ee95eb3231298f65507554edd0e173152b5
Megha Kukreti <meghakukreti0000@gmail.com>
2026-06-05 15:55:49 +0530

    Update q1.c

--- Day-5/q1.c
+++ Day-5/q1.c
@@ -4,7 +4,7 @@ int main(){
     printf("enter a number");
     scanf("%d",&n);
     int og=n;
-    for (i=1;i<n;i++){
+    for (i=1;i<n;i++){ //loop for finding the factors of the number
         if (og%i==0){
             sum=sum+i;
         }
