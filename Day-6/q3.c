b523266db396e4a1335be169c430c2b6dfb10b05
Megha Kukreti <meghakukreti0000@gmail.com>
2026-06-06 07:37:47 +0530

    Create q3.c

---
+++ Day-6/q3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n,count=0;
+    printf ("enter a number");
+    scanf ("%d",&n);
+    while (n>0){
+        if (n%2==1){
+            count++;
+        }
+    n =n/2;
+    }
+    printf ("the number of set bit are %d",count);
+    return 0;
+}
