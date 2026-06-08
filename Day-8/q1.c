68dde52926116868f2838c1885e9ea6f8bee54d0
Megha Kukreti <meghakukreti0000@gmail.com>
2026-06-08 17:51:22 +0530

    Update q1.c

--- Day-8/q1.c
+++ Day-8/q1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int n,i,j;
-    printf("enter a number");
+    printf("enter number of rows");
     scanf ("%d",&n);
     for (i=1;i<=n;i++){
         for (j=1;j<=i;j++){
