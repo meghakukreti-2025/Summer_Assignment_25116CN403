67cfde8a0e0cf27c4f82e2afdb1e8795829d3e11
Megha Kukreti <meghakukreti0000@gmail.com>
2026-06-19 21:19:16 +0530

    Create q2.c

---
+++ Day-19/q2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+    int m,n,i,j;
+    printf("enter number of rows and columns");
+    scanf("%d %d",&m,&n);
+    int a[m][n],b[m][n],c[m][n];
+    printf("enter elements of first matrix =");
+    for(i=0; i< m;i++) {
+        for(j =0;j< n;j++) {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("enter elements of second matrix=");
+    for(i =0;i< m;i++) {
+        for(j=0;j< n;j++) {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i =0;i< m;i++) {
+        for(j=0;j< n;j++) {
+            c[i][j] =a[i][j]-b[i][j];
+        }
+    }
+    printf("subtraction of matrices=\n");
+    for(i =0;i< m;i++) {
+        for(j=0;j<n;j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+return 0;
+}
