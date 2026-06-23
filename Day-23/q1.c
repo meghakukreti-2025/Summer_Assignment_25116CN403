62e7f693cd200a50174fff6305567f26750f3b09
Megha Kukreti <meghakukreti0000@gmail.com>
2026-06-23 21:35:00 +0530

    Create q1.c

---
+++ Day-23/q1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    char s[100];
+    int i,j,count;
+    printf("enter a string ");
+    scanf("%s",s);
+    for(i=0;s[i]!='\0';i++){
+        count=0;
+        for(j=0;s[j]!='\0';j++){
+            if(s[i]==s[j]){
+                count++;
+            }
+        }
+        if(count==1){
+            printf("first non repeating character=%c",s[i]);
+            break;
+        }
+    }
+return 0;
+}
