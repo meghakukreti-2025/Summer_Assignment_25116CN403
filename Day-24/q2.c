7f72c4bc21359c817a5830d44575165c996d0515
Megha Kukreti <meghakukreti0000@gmail.com>
2026-06-24 21:58:25 +0530

    Create q2.c

---
+++ Day-24/q2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char s[100];
+    int i,count;
+    printf("enter a string ");
+    scanf("%s",s);
+    for(i=0;s[i]!='\0';i=i+count){
+        count=1;
+        while(s[i]==s[i+count]){
+            count++;
+        }
+        printf("%c%d",s[i],count);
+    }
+    return 0;
+}
