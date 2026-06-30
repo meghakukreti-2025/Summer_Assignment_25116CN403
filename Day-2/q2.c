dd4788eace1b08a340674668332e1f04dec45b72
Megha Kukreti <meghakukreti0000@gmail.com>
2026-06-30 21:55:29 +0530

    Update q2.c

--- Day-2/q2.c
+++ Day-2/q2.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 int main(){
-    int n,new,rev=0;
-    printf ("enter the number");
+    int n,new,rev=0,sign=1;
+    printf("enter the number ");
     scanf("%d",&n);
-    while (n!=0){
-        new = n%10;
-        rev= rev*10 + new;
+    if(n<0){
+        sign=-1;
+        n=-n;
+    }
+    while(n!=0){
+        new=n%10;
+        rev=rev*10+new;
         n=n/10;
     }
-printf("the reverse of the number is=%d",rev);
+    rev=rev*sign;
+    printf("the reverse of the number is = %d",rev);
 return 0;
 }
