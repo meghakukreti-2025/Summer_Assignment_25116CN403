2ebc35f62b0d976edb20c1873958cc6c367c7d50
Megha Kukreti <meghakukreti0000@gmail.com>
2026-06-30 21:50:06 +0530

    Update q2.c

--- Day-30/q2.c
+++ Day-30/q2.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-struct book
-{
+struct book{
     int id;
     char name[30];
     char author[30];
 };
-int main()
-{
+int main(){
     struct book b[100];
     int n,i,search,f=0;
     printf("enter number of books ");
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++){
         printf("\nbook ID ");
         scanf("%d",&b[i].id);
         printf("book Name ");
@@ -22,18 +19,15 @@ int main()
         scanf("%s",b[i].author);
     }
     printf("\nBook Records\n");
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++){
         printf("\nID = %d",b[i].id);
         printf("\nBook = %s",b[i].name);
         printf("\nAuthor = %s\n",b[i].author);
     }
     printf("\nenter book ID to search ");
     scanf("%d",&search);
-    for(i=0;i<n;i++)
-    {
-        if(b[i].id==search)
-        {
+    for(i=0;i<n;i++){
+        if(b[i].id==search){
             printf("\nBook found");
             printf("\n%s",b[i].name);
             printf("\n%s\n",b[i].author);
